de30c39f3c6453cca84cb512aa9087712b32e541
Rohith M <rohith.m068@gmail.com>
2023-07-13 13:43:37 +0530

    Creating RabbitMQ server with proper vpc and accesing through bastian node {workstation)

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "component" {}
+variable "env" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "vpc_id" {}
+variable "tags" {}
+variable "sg_subnet_cidr" {}
+variable "allow_ssh_cidr" {}
