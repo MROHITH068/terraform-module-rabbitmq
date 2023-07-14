1f41c77870de93cf2ffd5ceb4212406a5017373c
Rohith M <rohith.m068@gmail.com>
2023-07-14 23:41:15 +0530

    Adding docdb and elasticache subnet groups

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "main" {
     name        = "${var.component}-${var.env}-sg"
     description = "${var.component}-${var.env}-sg"
     vpc_id = var.vpc_id
