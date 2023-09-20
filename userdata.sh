ff270a6752389c2486b6d9382439f9137ca957d4
Rohith M <rohith.m068@gmail.com>
2023-09-20 01:54:51 -0500

    Updated userdata.sh in rabbitmq module

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set-hostname -skip-apply ${component}
 labauto ansible
 ansible-pull -i localhost, -U https://github.com/MROHITH068/roboshop-ansible.git main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
